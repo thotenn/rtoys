d646f365054c5bfaba721ae677ff03e39ab1f01e
thotenn <joseenriquegaray@hotmail.com>
2023-09-21 14:24:29 -0400

    configure tests

---
+++ tests/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import { FetchSuspense } from '../src';
+
+class App extends Component{
+   render(){
+      return(
+         <div>
+            {/* <FetchSuspense > */}
+                <h1>Hello</h1>
+            {/* </FetchSuspense> */}
+         </div>
+      );
+   }
+}
+export default App;
